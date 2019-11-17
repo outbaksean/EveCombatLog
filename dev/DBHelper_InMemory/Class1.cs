62baadc6b60821bd96555ffc3481014b81833e61
Sean <seanehrig@gmail.com>
2019-11-17 17:08:03 -0500

    Stub DBHelper In Memory

---
+++ dev/DBHelper_InMemory/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using EveCombatLogModel;
+
+namespace DBHelper_InMemory
+{
+    public class DBHelper : IDBHelper
+    {
+
+        private Dictionary<string, Fitting> _fittings;
+
+        public void CreateCombatLogEntry(string fittingName, CombatLogEntry combatLogEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateFitting(Fitting fitting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCombatLogEntry(string combatLogEntryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFitting(string fittingName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fitting> GetAllFittings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CombatLogEntry> GetCombatLogEntries(string fittingName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CombatLogEntry GetCombatLogEntry(string combatLogEntryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fitting GetFitting(string fittingName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCombatLogEntry(string combatLogEntryName, CombatLogEntry combatLogEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFitting(string fittingName, Fitting fitting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dev/EveCombatLogModel/IDBHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EveCombatLogModel
+{
+    public interface IDBHelper
+    {
+        public void CreateFitting(Fitting fitting);
+
+        public List<Fitting> GetAllFittings();
+
+        public Fitting GetFitting(string fittingName);
+
+        public void UpdateFitting(string fittingName, Fitting fitting);
+
+        public void DeleteFitting(string fittingName);
+
+        public void CreateCombatLogEntry(string fittingName, CombatLogEntry combatLogEntry);
+
+        public List<CombatLogEntry> GetCombatLogEntries(string fittingName);
+
+        public CombatLogEntry GetCombatLogEntry(string combatLogEntryName);
+
+        public void UpdateCombatLogEntry(string combatLogEntryName, CombatLogEntry combatLogEntry);
+
+        public void DeleteCombatLogEntry(string combatLogEntryName);
+    }
+}
